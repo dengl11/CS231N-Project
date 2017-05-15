fa625778bab0681ff9f8b0c6e7e037e5c001a4c0
LI DENG <dengl11@foxmail.com>
2017-05-14 18:04:37 -0700

    add parser and comment

--- util/img_kit.py
+++ util/img_kit.py
@@ -1,6 +1,12 @@
+############################################################
+###############  Toolkit for Image Operations  #############
+############################################################
+
 def merge(end_points, mid):
+	"""
+	merge generated middle images with end-point images
+	"""
 	assert len(end_points) == len(mid) + 1, "len(end_points) == len (mid) + 1 not satisfied!"
 	merged = end_points[:]
-	for i, img in enumerate(mid):
-		merged.insert(2*i+1, img)
+	for i, img in enumerate(mid): merged.insert(2*i+1, img)
 	return merged
