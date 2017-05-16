13313671fbcd34ada4eece5d86ed1e1b6e6ffad4
LI DENG <dengl11@foxmail.com>
2017-05-16 11:55:20 -0700

    p2p

--- util/img_kit.py
+++ util/img_kit.py
@@ -23,18 +23,20 @@ def merge(end_points, mid):
 
 def create_dir(directory):
 	try: os.stat(directory)
-	except: os.mkdir(directory)
+	except: 
+		print("{} not existing. Just created!".format(directory))
+		os.mkdir(directory)
 
 
-def resize(img_path, size, output_folder):
+def resize(img_path, size, output_folder, keep_ratio=False):
 	"""
 	resize single image from path, to output_folder
 	size: (width, hight)
 	"""
-
 	try:
 		im = Image.open(img_path)
-		im.thumbnail(size, Image.ANTIALIAS)
+		if keep_ratio: im.thumbnail(size, Image.ANTIALIAS)
+		else:          im.thumbnail(size)
 		im.save(os.path.join(output_folder, img_path.split("/")[-1]), "JPEG")
 	except IOError:
 		print("cannot create thumbnail for '%s'" % img_path)
@@ -48,4 +50,8 @@ def resize_all(input_folder, output_folder, size):
 	create_dir(output_folder)
 	for m in imgs_in_folder(input_folder):
 		resize(os.path.join(input_folder, m), size, output_folder)
+
+
+def avg_imges(x1, x2, dtype='uint8'):
+    return np.array([x1, x2]).mean(axis=0).astype(dtype)
  	
