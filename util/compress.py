df6db244048ec5d10c6b932227c812bfabc2403f
sijunhe <sijun.he@hotmail.com>
2017-05-29 16:15:15 -0700

    new compressor

--- util/compress.py
+++ util/compress.py
@@ -4,9 +4,17 @@ import numpy as np
 
 img_src = 'data/kitti_data/'
 file_names = sorted(glob.glob(img_src + '/*/*.png', recursive=True))
-data = np.concatenate([misc.imread(i)[np.newaxis, ...] for i in file_names], axis=0)
+count = 0
+resized_img_array = []
+for i in file_names:
+	resized_img_array.append(misc.imresize(misc.imread(i), (128, 384)))
+	count += 1
+	if count % 1000 == 0:
+		print("Processed {} images".format(count))
+data = np.concatenate([i[np.newaxis, ...] for i in resized_img_array], axis=0)
+
 print("Data loaded! Shape: {}".format(data.shape))
-save_path = "data/kitti-compressed"
+save_path = "data/kitti-compressed-resized"
 np.save(save_path, data)
 print("Saved to:  {}".format(save_path))
 		
